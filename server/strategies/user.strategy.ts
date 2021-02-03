9c4f14c97a90aff4de8afa91df11f8686743385d
Myron <dubtwee@gmail.com>
2021-02-03 10:26:29 -0600

    converting the User Strategy to a TS file

--- server/strategies/user.strategy.ts
+++ server/strategies/user.strategy.ts
@@ -1,16 +1,16 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const encryptLib = require('../modules/encryption');
-const pool = require('../modules/pool');
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import encryptLib from '../modules/encryption';
+import pool from '../modules/pool';
 
-passport.serializeUser((user, done) => {
+passport.serializeUser((user: any, done: any): void => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
+passport.deserializeUser((id: string | number, done: any): void => {
   pool
     .query('SELECT * FROM "user" WHERE id = $1', [id])
-    .then((result) => {
+    .then((result: any): void => {
       // Handle Errors
       const user = result && result.rows && result.rows[0];
 
@@ -26,7 +26,7 @@ passport.deserializeUser((id, done) => {
         done(null, null);
       }
     })
-    .catch((error) => {
+    .catch((error: string): void => {
       console.log('Error with query during deserializing user ', error);
       // done takes an error (we have one) and a user (null in this case)
       // this will result in the server returning a 500 status code
@@ -37,29 +37,31 @@ passport.deserializeUser((id, done) => {
 // Does actual work of logging in
 passport.use(
   'local',
-  new LocalStrategy((username, password, done) => {
-    pool
-      .query('SELECT * FROM "user" WHERE username = $1', [username])
-      .then((result) => {
-        const user = result && result.rows && result.rows[0];
-        if (user && encryptLib.comparePassword(password, user.password)) {
-          // All good! Passwords match!
-          // done takes an error (null in this case) and a user
-          done(null, user);
-        } else {
-          // Not good! Username and password do not match.
-          // done takes an error (null in this case) and a user (also null in this case)
-          // this will result in the server returning a 401 status code
-          done(null, null);
-        }
-      })
-      .catch((error) => {
-        console.log('Error with query for user ', error);
-        // done takes an error (we have one) and a user (null in this case)
-        // this will result in the server returning a 500 status code
-        done(error, null);
-      });
-  })
+  new LocalStrategy(
+    (username: string, password: string, done: Function): void => {
+      pool
+        .query('SELECT * FROM "user" WHERE username = $1', [username])
+        .then((result: any): void => {
+          const user = result && result.rows && result.rows[0];
+          if (user && encryptLib.comparePassword(password, user.password)) {
+            // All good! Passwords match!
+            // done takes an error (null in this case) and a user
+            done(null, user);
+          } else {
+            // Not good! Username and password do not match.
+            // done takes an error (null in this case) and a user (also null in this case)
+            // this will result in the server returning a 401 status code
+            done(null, null);
+          }
+        })
+        .catch((error: any): void => {
+          console.log('Error with query for user ', error);
+          // done takes an error (we have one) and a user (null in this case)
+          // this will result in the server returning a 500 status code
+          done(error, null);
+        });
+    }
+  )
 );
 
 module.exports = passport;
