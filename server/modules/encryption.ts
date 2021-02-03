31de600898d3f3929b41c742ce12bfbe9692fdd1
Myron <dubtwee@gmail.com>
2021-02-03 12:26:39 -0600

    converting the encryption module to TS

--- server/modules/encryption.ts
+++ server/modules/encryption.ts
@@ -1,17 +1,20 @@
 // No changes should be required in this file
 
-const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs';
 
-const SALT_WORK_FACTOR = 10; // This determines how secure the salt should be
+const SALT_WORK_FACTOR: number = 10; // This determines how secure the salt should be
 
-const encryptPassword = (password) => {
-  const salt = bcrypt.genSaltSync(SALT_WORK_FACTOR); // This generates a random salt
+export const encryptPassword = (password: string): string => {
+  const salt: string = bcrypt.genSaltSync(SALT_WORK_FACTOR); // This generates a random salt
   // This next line hashes the user password and the random salt
   // this salt and hash (and not the actual password) will then get stored in the database
   return bcrypt.hashSync(password, salt);
 };
 
-const comparePassword = (candidatePassword, storedPassword) => {
+export const comparePassword = (
+  candidatePassword: string,
+  storedPassword: string
+): boolean => {
   /*
   This takes in the candidate password (what the user entered) to check it.
   The stored password has the original salt, so it will run the
@@ -20,9 +23,4 @@ const comparePassword = (candidatePassword, storedPassword) => {
   If this interests you, check out this video https://www.youtube.com/watch?v=8ZtInClXe1Q
   */
   return bcrypt.compareSync(candidatePassword, storedPassword);
-};
-
-module.exports = {
-  encryptPassword,
-  comparePassword,
 };
