f657d5748ae585745d53ef8d440f6dfcaa8ca375
DoctorHowser <doctorhowser@gmail.com>
2021-02-26 09:33:05 -0600

    fix

--- server/routes/book.router.ts
+++ server/routes/book.router.ts
@@ -3,19 +3,18 @@ import pool from '../modules/pool'
 
 const router: express.Router = express.Router();
 
-const BookServiceInstance : BookService = new BookService(pool);
 
-router.get('/', async (req: Request, res: Response) : Promise<void>  => {
+router.get('/', async (req,res)  => {
 
 
 })
 
-router.delete('/:id', async (req: Request, res: Response) : Promise<void>  => {
+router.delete('/:id', async (req,res)  => {
 
 
 });
 
-router.post('/', async (req: Request, res: Response) : Promise<void>  => {
+router.post('/', async (req,res)  => {
 
 
 });
